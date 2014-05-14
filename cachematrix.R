14f29aabb96e816be84f6b55deec236ef1721b02
sgourkar <gourkar.s@gmail.com>
2014-05-13 18:36:58 -0700

    cacheMatrix.R
    Progress Assignment 2

--- cachematrix.R
+++ cachematrix.R
@@ -13,3 +13,28 @@ makeCacheMatrix <- function(x = matrix()) {
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
 }
+## These two functions work to cache the inverse of a matrix in order to save time if such a task is 
+## required more than once (e.g. in a loop)
+
+# makeCacheMatrix takes a matrix as an argument and calculates it's inverse. The inverse matrix is defined 
+# in the global environment with the "<<-" operator
+
+
+makeCacheMatrix <- function(x = matrix()) {
+        InvMatrix <<- solve(x) ## Calculate inverse of the matrix 'x'
+        
+}
+
+
+## cacheSolve looks to see if the inverse of the matrix argument has already been calculated in the 
+## parent environment, and if so, returns that inverse matrix. If the inverse matrix has not been calculated,
+## then it is calculated and returned in this function.
+cacheSolve <- function(x) {
+        if (!is.null(InvMatrix)) {
+                return(InvMatrix)       ## IF InvMatrix exists, it is returned here
+        } else {
+                InvMatrix <- solve(x)   ## InvMatrix is calculated if it doesn't already exist in the global
+                                        ## environment
+        }
+        return(InvMatrix)               ## InvMatrix is returned here.
+}
